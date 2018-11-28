78fabd351755258c1a81ef8eb430b34734d0f600
Susmitha George <Susmitha.George@trv.envestnet.com>
2018-11-28 18:58:40 +0530

    Added transactions package : Susmitha
    to classify and load transactions

---
+++ SpendWise/src/transactions/Transactions.java
@@ -0,0 +1,14 @@
+package transactions;
+
+public class Transactions {
+	
+	private java.util.ArrayList<TransactionDTO> transaction;
+
+	public java.util.ArrayList<TransactionDTO> getTransactions() {
+		return transaction;
+	}
+
+	public void setTransactions(java.util.ArrayList<TransactionDTO> transaction) {
+		this.transaction = transaction;
+	}
+}
