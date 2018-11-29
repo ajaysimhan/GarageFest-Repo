f0b93e742e8716d19ee187db0cf99522a2d5c4bf
Benny P P <Benny.Padayattil@trv.envestnet.com>
2018-11-29 15:34:51 +0530

    Changes for dynamic pie chart

--- SpendWise/src/transactions/TransactionClassifier.java
+++ SpendWise/src/transactions/TransactionClassifier.java
@@ -3,6 +3,7 @@ package transactions;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -16,16 +17,26 @@ public class TransactionClassifier {
 	
 	ArrayList<Integer> usefulCategories = null;
 	ArrayList<Integer> nonUsefulCategories = null;
+	
 	ArrayList<TransactionDTO> transactions = null;
+	ArrayList<CategoryDTO> categories = null;
 
-	public TransactionClassifier(){
-		init();
+	HashMap<Integer, TransactionDTO> usefultransactions;
+	HashMap<Integer, TransactionDTO> nonUsefultransactions;
+	
+	public TransactionClassifier()
+	{
+		init(); 
 	}
 	
-	private void init() {
+	private void init() 
+	{
 		loadAllTransactions();
-		loadUsefulCategories();
-		loadNonUsefulCategories();
+		loadAllCategories();
+		
+		classifyCategories();
+		classifyTransactions();
+		
 		System.out.println("useful" + getUsefulTransactionAmount());
 		System.out.println("non useful" + getNonUsefulTransactionAmount());
 	}
@@ -42,24 +53,60 @@ public class TransactionClassifier {
 		transactions = bulidTransactionDTO(allTransactions);
 	}
 	
-	public void loadUsefulCategories() 
+	public void loadAllCategories() 
 	{
-		usefulCategories = new ArrayList<Integer>();
-		usefulCategories.add(17);
-		usefulCategories.add(45);
+		String allCategories = null;
+		try {
+			allCategories = FileUtils
+					.readFileToString(new File(BasicInfoModel.FILE_PATH + "/transactions/categories.txt")).trim();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		categories = bulidCategoryDTO(allCategories);
 	}
 	
-	public void loadNonUsefulCategories() 
+	public void classifyCategories() 
 	{
+		usefulCategories = new ArrayList<Integer>();
 		nonUsefulCategories = new ArrayList<Integer>();
-		nonUsefulCategories.add(32);
+		for (CategoryDTO c : categories) {
+			if (c.getIndicator().equals("U")) {
+				usefulCategories.add(c.getCategoryId());
+			}
+			else if (c.getIndicator().equals("N")) {
+				nonUsefulCategories.add(c.getCategoryId());
+			}
+		}		
+	}
+	
+	public void classifyTransactions() 
+	{
+		usefultransactions = new HashMap<Integer, TransactionDTO>();
+		nonUsefultransactions = new HashMap<Integer, TransactionDTO>();
+		for (TransactionDTO t: transactions) {
+			for (CategoryDTO c : categories) {
+				if(t.getCategoryId() == c.getCategoryId()) {
+					if (c.getIndicator().equals("U")) {
+						usefultransactions.put(c.getCategoryId(), t);
+					}
+					else if (c.getIndicator().equals("N")) {
+						nonUsefultransactions.put(c.getCategoryId(), t);
+					}
+				}
+			}
+		}
 	}
 	
 	private static ArrayList<TransactionDTO> bulidTransactionDTO(String allTransactions)
 	{
-		ArrayList<TransactionDTO> dtos = new ArrayList<>();
 		Transactions transactions = new Gson().fromJson(allTransactions, Transactions.class);
 		return transactions.getTransactions();
+	}
+	
+	private static ArrayList<CategoryDTO> bulidCategoryDTO(String allCategories)
+	{
+		Categories categories = new Gson().fromJson(allCategories, Categories.class);
+		return categories.getCategories();
 	}
 	
 	public double getTotalTransactionAmount(){
@@ -71,22 +118,34 @@ public class TransactionClassifier {
 	}
 	
 	public double getUsefulTransactionAmount(){
+				
 		double usefulTransactionAmount= 0;
-		for (TransactionDTO t: transactions) {
-            if(usefulCategories.contains(t.getCategoryId())) {
+		for (TransactionDTO t: usefultransactions.values()) {
             	usefulTransactionAmount += t.getAmount().getAmount();
-            }
         }
 		return usefulTransactionAmount;
 	}
 	
 	public double getNonUsefulTransactionAmount(){
 		double nonUsefulTransactionAmount= 0;
-		for (TransactionDTO t: transactions) {
-            if(nonUsefulCategories.contains(t.getCategoryId())) {
+		for (TransactionDTO t: nonUsefultransactions.values()) {
             	nonUsefulTransactionAmount += t.getAmount().getAmount();
-            }
         }
 		return nonUsefulTransactionAmount;
+	}
+	
+	public HashMap<String, Integer> getTransactionsForChart(){
+		HashMap<String, Integer> chartTransactions = new HashMap<String, Integer>();
+		int amount;
+		for (TransactionDTO t: nonUsefultransactions.values()) {
+			for (CategoryDTO c : categories) {
+				if(t.getCategoryId() == c.getCategoryId()) {
+					amount = (int) t.getAmount().getAmount();
+					chartTransactions.put(c.getDescription(), amount);
+				}
+			}
+		}
+		System.out.println(chartTransactions.toString());
+		return chartTransactions;
 	}
 }
