d66728cb63b783ffd15c5e5056747896a965e9a3
Ajay Simhan A J <Ajay.Jayasimhan@trv.envestnet.com>
2018-11-29 15:10:01 +0530

    Chart

---
+++ SpendWise/WebContent/js/app.js
@@ -0,0 +1,584 @@
+Highcharts.chart('1', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
+
+Highcharts.chart('2', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
+
+Highcharts.chart('3', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
+
+Highcharts.chart('4', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
+
+Highcharts.chart('5', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
+
+Highcharts.chart('6', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
+
+Highcharts.chart('7', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
+
+Highcharts.chart('8', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
+
+Highcharts.chart('9', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
+
+Highcharts.chart('10', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
+
+Highcharts.chart('11', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
+
+Highcharts.chart('12', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
+
+Highcharts.chart('13', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'Product NAme'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Investment share',
+        data: [
+            ['pd1', 45.0],
+            ['pd2', 26.8],
+            {
+                name: 'pd3',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['pd4', 8.5],
+            ['pd5', 6.2],
+            ['pd6', 0.7]
+        ]
+    }]
+});
